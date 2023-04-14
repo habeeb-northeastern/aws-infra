e54fe98a268c999bebb435392296fee4d26f2a0b
Habeebuddin Mir <habeebuddinmir@gmail.com>
2023-04-14 00:36:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,12 @@ module "myNetwork" {
   cidr            = "10.0.0.0/16"
   region          = "us-east-1"
   available_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  profile         = "prod"
+  profile         = "demo"
   ami_id          = "ami-02badb092d4d180e3"
   app_port        = "8080"
   password        = "Coffeebites1$"
   name            = "csye6225"
-  AWS_KEY         = "AKIASEE4BOWZQWZFG24R"
-  AWS_ID          = "SW/dtHWwa+STX99V1o9jnSPbID0ahYpwZSpV8bLy"
+  AWS_KEY         = "SW/dtHWwa+STX99V1o9jnSPbID0ahYpwZSpV8bLy"
+  AWS_ID          = "AKIASEE4BOWZQWZFG24R"
 
 }
-
-//module "myNetwork2" {
-//source          = "./module/networking"
-//cidr            = "10.0.0.0/16"
-//region          = "us-west-2"
-//  available_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-// profile         = "dev"
-
-//}
