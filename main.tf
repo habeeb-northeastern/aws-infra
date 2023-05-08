43aca0a3c089ac8e27280a5f319fc12ce285e68f
Habeebuddin Mir <habeebuddinmir@gmail.com>
2023-05-08 00:26:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ module "myNetwork" {
   profile         = "demo"
   ami_id          = "ami-02badb092d4d180e3"
   app_port        = "8080"
-  password        = "Coffeebites1$"
+  password        = ""
   name            = "csye6225"
-  AWS_KEY         = "SW/dtHWwa+STX99V1o9jnSPbID0ahYpwZSpV8bLy"
-  AWS_ID          = "AKIASEE4BOWZQWZFG24R"
+  AWS_KEY         = ""
+  AWS_ID          = ""
 
 }
